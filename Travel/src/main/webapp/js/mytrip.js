c8c8848e7cbedd5fbf910b7534b1b8fb78e937de
jinho1995 <yangjunho1995@gmail.com>
2024-06-17 17:46:58 +0900

    mytrip.js 추가

---
+++ Travel/src/main/webapp/js/mytrip.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var btnSchedule = document.getElementById("btn_shcedule");
+var btn1 = document.getElementById("btn_1");
+var btn2 = document.getElementById("btn_2");
+var btn3 = document.getElementById("btn_3");
+var btn4 = document.getElementById("btn_4");
+
+    btn1.addEventListener("click", function() {
+        window.location.assign("addschedule");
+    });
+
+    btn2.addEventListener("click", function() {
+        window.location.assign("mytrip");
+    });
+
+    btn3.addEventListener("click", function() {
+        window.location.assign("");
+    });
+
+    btn4.addEventListener("click", function() {
+        window.location.assign("");
+    });
+
+var thumbnailclick= document.selectQquerySelectorAll('.thumbnail');
+    thumbnailclick.addEventListener('click', function(){
+		
+	})
+});

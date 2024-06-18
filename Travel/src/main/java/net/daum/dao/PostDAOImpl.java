b3daddf1ca5841c1e118b1d2d80adc06578f6cc5
여준서 <duwnstj12@naver.com>
2024-06-18 17:00:47 +0900

    새 브런치 duwnstj 생성

--- Travel/src/main/java/net/daum/dao/PostDAOImpl.java
+++ Travel/src/main/java/net/daum/dao/PostDAOImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 

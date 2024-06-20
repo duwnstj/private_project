133602689bf6d0d4d37beb79b8346b13ae735fc5
jinho1995 <yangjunho1995@gmail.com>
2024-06-20 20:42:21 +0900

    240620 수정본2_양준호

--- Travel/src/main/java/net/daum/vo/PlanVO.java
+++ Travel/src/main/java/net/daum/vo/PlanVO.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
